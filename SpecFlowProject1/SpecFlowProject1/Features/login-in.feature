19c38f94c1e5887ebfa820abe54671200d0969db
Krzysztof Gibert <krzysztof.gibert@intive.com>
2021-08-03 10:51:00 +0200

    fdfd

---
+++ SpecFlowProject1/SpecFlowProject1/Features/login-in.feature
@@ -0,0 +1,20 @@
+Feature: Login in
+
+  desc
+
+  Background:
+    Given ms
+    When as
+    Then tr
+
+  @TM-1 @MANUAL @COMPLETED
+  Scenario: scenario_1
+    Given ms
+    When as
+    Then tr
+
+  @TM-1 @COMPLETED
+  Scenario: scenario_2
+    Given wp.pl openned
+    When correct credentials are filled
+    Then user is logged in
---
+++ SpecFlowProject1/SpecFlowProject1/Steps/LoginInSteps.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject1.Steps
+{
+    [Binding]
+    public class LoginInSteps
+    {
+        [Given(@"ms")]
+        public void GivenMs()
+        {
+            Assert.Fail("kg message");
+        }
+        
+        [Given(@"wp\.pl openned")]
+        public void GivenWp_PlOpenned()
+        {
+            Assert.Fail("kg message");
+        }
+        
+        [When(@"as")]
+        public void WhenAs()
+        {
+            Assert.Fail("kg message");
+        }
+        
+        [When(@"correct credentials are filled")]
+        public void WhenCorrectCredentialsAreFilled()
+        {
+            Assert.Fail("kg message");
+        }
+        
+        [Then(@"tr")]
+        public void ThenTr()
+        {
+            Assert.Fail("kg message");
+        }
+        
+        [Then(@"user is logged in")]
+        public void ThenUserIsLoggedIn()
+        {
+            Assert.Fail("kg message");
+        }
+    }
+}
